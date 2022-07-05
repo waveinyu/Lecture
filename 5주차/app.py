411fda0f80bc28d576c75b16682400fd69370760
waveinyu <99732695+waveinyu@users.noreply.github.com>
2022-07-05 23:33:26 +0900

    220705 Update

--- 5주차/app.py
+++ 5주차/app.py
@@ -28,12 +28,19 @@ def bucket_post():
 
     return jsonify({'msg': '버킷리스트 저장 완료!'})
 
-
+# 데이터가 클라이언트에서 서버로 날아올 땐 String으로 온다
 @app.route("/bucket/done", methods=["POST"]) # done_bucket(num)
 def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one({'num':int(num_receive)},{'$set':{'done':1}})
-    return jsonify({'msg': '버킷 완료!'})
+    done_receive = request.form['done']
+    num_receive = request.form['num_give'] # print(num_receive)
+    # print(type(num_receive))
+    
+    if (int(done_receive) == 0): 
+        db.bucket.update_one({'num':int(num_receive)},{'$set':{'done':1}})
+        return jsonify({'msg': '버킷 완료!'})     
+    else:
+        db.bucket.update_one({'num':int(num_receive)},{'$set':{'done':0}})
+        return jsonify({'msg': '취소 완료!'})    
 
 
 # 전체 버킷리스트 보여주기 API
@@ -41,6 +48,7 @@ def bucket_done():
 def bucket_get():
     bucket_list = list(db.bucket.find({},{'_id':False}))
     return jsonify({'buckets': bucket_list})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
